0104964478643d4335b8f7e3a2329c73d8c564bc
jun <Julian.Nagel@ti8m.ch>
2024-06-19 13:24:32 +0200

    Added some features to "CreateTable" class. However, im not finished.

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -1,9 +1,58 @@
-public class CreateTable{
+import java.util.Scanner;
+
+public class CreateTable {
     String tableQuery;
-    public void defineTableQuerry(){
+    Scanner scanner = new Scanner(System.in);
+
+    public void defineTableQuerry() {
+        System.out.println("Enter name of the entity");
+        String tableName = scanner.next();
+
+
         StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("CREATE TABLE IF NOT EXISTS");
+
+        queryBuilder.append("CREATE TABLE IF NOT EXISTS ").append(tableName).append(" (\n");
+
+
+        while (true) {
+            System.out.println("Add column? (y/n)");
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("y")) {
+                queryBuilder.append(defineColumn());
+            } else if (input.equalsIgnoreCase("n")) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter 'y' or 'n'.");
+            }
+        }
+
+        queryBuilder.append(");");
+
         this.tableQuery = queryBuilder.toString();
     }
 
+    public String defineColumn() {
+        StringBuilder collumnBuilder = new StringBuilder();
+
+        System.out.println("Enter column name.");
+        collumnBuilder.append(scanner.next());
+
+        System.out.println("Enter datatype.");
+        collumnBuilder.append(" ").append(scanner.next());
+
+        System.out.println("Enter Length.");
+        collumnBuilder.append("(").append(scanner.nextInt()).append(")");
+
+        System.out.println("Primary Key?(y/n)");
+        if(scanner.next().equals("y")){
+            collumnBuilder.append(" PRIMARY KEY AUTO_INCREMENT");
+        }
+
+
+
+
+        System.out.println();
+
+        return collumnBuilder.toString();
+    }
 }
